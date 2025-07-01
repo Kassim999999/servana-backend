a1ba66aab3a96c1124574b9fd5dd4239f0880031
Kassim <kassimrooney@gmail.com>
2025-07-01 11:39:45 +0300

    initialized flask

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+    
+    CORS(app)
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from app.routes.auth_routes import auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/api/auth')
+    
+    return app
