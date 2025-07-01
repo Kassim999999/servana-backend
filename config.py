f1071a3db309b226444726250e83c0987eb59f14
Kassim <kassimrooney@gmail.com>
2025-07-01 13:12:10 +0300

    Updated the database and config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'instance', 'dev.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-secret")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret")
--- run.py
+++ run.py
@@ -1,10 +1,27 @@
 from app import create_app, db
-from app.models import User
+from app.models import User, Service
+
+
+
+def seed_services():
+    if not Service.query.first():
+        mock_services = [
+            Service(name="Home Cleaning", category="Cleaning", price="KSh 1500", icon="🧹"),
+            Service(name="Sofa Cleaning", category="Cleaning", price="KSh 3000", icon="🛋️"),
+            Service(name="Tap Repair", category="Plumbing", price="KSh 800", icon="🔧"),
+            Service(name="Hair Styling", category="Beauty", price="KSh 2000", icon="💇🏽‍♀️"),
+        ]
+        db.session.add_all(mock_services)
+        db.session.commit()
+        print("✅ Mock services seeded.")
 
 app = create_app()
 
 with app.app_context():
-    db.create_all()  # Create tables on first run
+    db.create_all()
+    print("📦 Tables created:", db.inspect(db.engine).get_table_names())  # ADD THIS
+    seed_services()
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
